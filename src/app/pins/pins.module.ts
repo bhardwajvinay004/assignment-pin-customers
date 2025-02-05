44efe76e23154a608053b471653012d5df711ea4
Vinay Bhardwaj <bhardwajvinay004@gmail.com>
2025-02-05 23:12:57 +0530

    installed packages, added bootstrap minified css fil path in angular.json and created 2 modules: Customers & Pins

---
+++ src/app/pins/pins.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PinsRoutingModule } from './pins-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PinsRoutingModule
+  ]
+})
+export class PinsModule { }
